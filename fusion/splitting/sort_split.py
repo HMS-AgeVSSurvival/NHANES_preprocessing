e338e6edc54f0b638aacda1367fb91900462abb4
Théo <theo.vincent@eleves.enpc.fr>
2021-05-27 17:00:12 +0000

    Alan change split examination

--- fusion/splitting/sort_split.py
+++ fusion/splitting/sort_split.py
@@ -2,14 +2,14 @@ import json
 
 
 def sort_split():
-    with open("fusion/splitting/splitting_examination.json") as json_file:
+    with open("fusion/splitting/split_examination.json") as json_file:
         splitting_examination = json.load(json_file)
 
     sorted_splitting_examination = {}
     for key, value in splitting_examination.items():
         sorted_splitting_examination[key] = sorted(value)
 
-    with open("fusion/splitting/splitting_examination.json", "w") as outfile:
+    with open("fusion/splitting/split_examination.json", "w") as outfile:
         json.dump(sorted_splitting_examination, outfile)
 
 
