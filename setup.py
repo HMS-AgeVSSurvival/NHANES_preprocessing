7aab305c6cdf4d1b3384d027cd90440f7a9924f0
Vincent <tv51@login01.o2.rc.hms.harvard.edu>
2021-06-01 12:00:30 -0400

    Clean examination

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ setup(
     name="nhanes-preprocessing",
     version="0.1",
     description="Preprocess the data from NHANES dataset.",
-    packages=["fusion"],
+    packages=["fusion", "nan_cleaning"],
     requires=["setuptools", "wheel"],
     install_requires=["numpy", "pandas", "pyarrow", "xport==2.0.2"],
-    extras_require={"dev": ["tqdm", "jupyter", "black"]},
+    extras_require={"dev": ["tqdm", "jupyter", "ipympl", "black", "matplotlib"]},
     entry_points={
         "console_scripts": [
             "fusion_examination=fusion.fusion:fusion_examination_cli",
+            "cleaning_examination=nan_cleaning.cleaning:cleaning_examination_cli",
         ]
     },
 )
