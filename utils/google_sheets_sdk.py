42f230b0d6b130474923bd92894f9810bc2b1d41
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2021-08-20 11:56:22 -0400

    Gspread accessor up to date

--- utils/google_sheets_sdk.py
+++ utils/google_sheets_sdk.py
@@ -2,7 +2,9 @@ import os
 import time
 import numpy as np
 import gspread
+import requests
 import json
+from json.decoder import JSONDecodeError
 
 
 def get_worksheet(main_category):
@@ -16,9 +18,17 @@ def get_worksheet(main_category):
 
 
 def handle_gspread_error(error):
-    error = json.loads(error.response._content)
+    try:
+        error = json.loads(error.response._content)
+    except JSONDecodeError:
+        raise error
 
-    if error["error"]["code"] == 429:  # Means too many Google Sheet API's calls
+    if error["error"]["code"] in [
+        404,
+        429,
+        101,
+        500,
+    ]:  # Means too many Google Sheet API's calls
         sleep_time = 61
         print(f"Sleep {sleep_time}")
         time.sleep(sleep_time)
@@ -26,67 +36,76 @@ def handle_gspread_error(error):
         raise error
 
 
-def update_cell(main_category, row, col, value):
+def update_cell(sheet_name, row, col, value):
     cell_updated = False
     while not cell_updated:
         try:
-            worksheet = get_worksheet(main_category)
-            print(np.float32(value))
+            worksheet = get_worksheet(sheet_name)
             worksheet.update_cell(row, col, str(value))
             cell_updated = True
         except gspread.exceptions.APIError as error_gspread:
             handle_gspread_error(error_gspread)
+        except requests.exceptions.ReadTimeout:
+            pass
 
 
-def find_cell(main_category, name):
+def find_cell(sheet_name, name):
     got_cell = False
     while not got_cell:
         try:
-            worksheet = get_worksheet(main_category)
+            worksheet = get_worksheet(sheet_name)
             cell = worksheet.find(name)
             got_cell = True
         except gspread.exceptions.APIError as error_gspread:
             handle_gspread_error(error_gspread)
+        except requests.exceptions.ReadTimeout:
+            pass
 
     return cell
 
 
-def findall_cells(main_category, name):
+def findall_cells(sheet_name, name):
     got_cell = False
     while not got_cell:
         try:
-            worksheet = get_worksheet(main_category)
+            worksheet = get_worksheet(sheet_name)
             cells = worksheet.findall(name)
             got_cell = True
         except gspread.exceptions.APIError as error_gspread:
             handle_gspread_error(error_gspread)
+        except requests.exceptions.ReadTimeout:
+            pass
 
     return cells
 
 
-def get_cell(main_category, row, col):
+def get_cell(sheet_name, row, col):
     got_cell = False
     while not got_cell:
         try:
-            worksheet = get_worksheet(main_category)
+            worksheet = get_worksheet(sheet_name)
             cell = worksheet.cell(row, col)
             got_cell = True
         except gspread.exceptions.APIError as error_gspread:
             handle_gspread_error(error_gspread)
+        except requests.exceptions.ReadTimeout:
+            pass
 
     return cell
 
 
-def get_col_values(main_category, col_name):
-    col = find_cell(main_category, col_name).col
+def get_col_values(sheet_name, col_name):
+    col = find_cell(sheet_name, col_name).col
 
     got_col = False
     while not got_col:
         try:
-            worksheet = get_worksheet(main_category)
+            worksheet = get_worksheet(sheet_name)
             col_values = worksheet.col_values(col)
             got_col = True
         except gspread.exceptions.APIError as error_gspread:
             handle_gspread_error(error_gspread)
+        except requests.exceptions.ReadTimeout:
+            pass
 
     return col_values
