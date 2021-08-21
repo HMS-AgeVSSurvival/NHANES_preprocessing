ee76e9531f9897cda50621491ecd30cf8d5c547d
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2021-08-21 03:49:52 -0400

    Fusion cleaned and working

--- fusion/shell_script/unit_fusion.sh
+++ fusion/shell_script/unit_fusion.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --partition short
-#SBATCH --time=01:00:00
+#SBATCH --time=00:10:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu 5G
+#SBATCH --mem-per-cpu 10G
 
 module load gcc/6.2.0
 module load python/3.7.4
