9c4c6cdc2363ac57dd0201d826a24bd7cf0450c6
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2021-08-21 04:33:33 -0400

    Merge working

--- merge/shell_script/run_merge.sh
+++ merge/shell_script/run_merge.sh
@@ -2,12 +2,17 @@
 
 for MAIN_CATEGORY in "examination" "laboratory" "questionnaire"
 do
+    [ -d out/merge/$MAIN_CATEGORY/ ] || mkdir -p out/merge/$MAIN_CATEGORY/
+    [ -d error/merge/$MAIN_CATEGORY/ ] || mkdir -p error/merge/$MAIN_CATEGORY/
+
     for PATH_CATEGORY in casting/data/$MAIN_CATEGORY/*
     do
         IFS='/' read -r a a a FILE_CATEGORY <<<"$PATH_CATEGORY"
         CATEGORY=$(echo $FILE_CATEGORY | cut -d "." -f 1)
 
-        rm out/merge/$MAIN_CATEGORY/$CATEGORY.out
-        sbatch -J merge/$MAIN_CATEGORY/$CATEGORY -o out/merge/$MAIN_CATEGORY/$CATEGORY.out merge/shell_script/unit_merge.sh -mc $MAIN_CATEGORY -c $CATEGORY
+        echo -n > out/merge/$MAIN_CATEGORY/$CATEGORY.out
+        echo -n > error/merge/$MAIN_CATEGORY/$CATEGORY.out
+
+        sbatch -J merge/$MAIN_CATEGORY/$CATEGORY -o out/merge/$MAIN_CATEGORY/$CATEGORY.out -e error/merge/$MAIN_CATEGORY/$CATEGORY.out merge/shell_script/unit_merge.sh -mc $MAIN_CATEGORY -c $CATEGORY
     done
 done
